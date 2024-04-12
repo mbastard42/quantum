ed72e8e3863e8d73c664e017cecfc4fab5270354
mbastard <matteobastard@gmail.com>
2024-04-12 04:00:00 +0200

    ex3

---
+++ files/3_entanglement.py
@@ -0,0 +1,17 @@
+from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
+from qiskit.visualization import plot_histogram
+
+circuit = QuantumCircuit(2)
+circuit.h(0)
+circuit.cx(0, 1)
+
+print(circuit.draw())
+
+circuit.measure_all()
+simulator = AerSimulator()
+
+job = simulator.run(circuit, shots=500)
+result = job.result()
+
+plt = plot_histogram(result.get_counts(), filename="tmp/ex3_histogram.png")
